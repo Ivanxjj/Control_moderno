b18ba11cf42b1458b1a426d99b59c6fd277b2a77
Ivanxjj <153119419+Ivanxjj@users.noreply.github.com>
2025-06-02 01:37:38 -0500

    t6 1er
    t6 1er

--- uno.m
+++ uno.m
@@ -1,27 +1,29 @@
-clc,clear all,close all;
+%Universida de Cuenca
+%Materia: Teoria de control meoderno
+%Taller 6: 
+%Autor: Ivan Javier Jara
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clc;clear all;close all;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Primer orden estable
 num=1;
-den=[2 1];
-gs=tf(num,den);
-roots(den);
-pole(gs);
-%%
-wn=1;
-xi=1.2;%0 osilatorio,0.2 subamoprtiguado,1 crticament amortiguado.1.2sobre amoritguado
+%den=[2 1];%estable
+den=[2 -1];%inestable
+tau=4*2;
+Gs=tf(num,den)
+[y,t]=step(Gs);
+disp("polos de Gs");
+roots(den)
 
-num=wn^2;
-den=[1 2*xi*wn wn^2];
-gs=tf(num,den);
+plot(t,y,DisplayName='Respuesta al escalon');
+title("Respuesta al escalon de un sitema de primer orden");
+grid minor;
+hold on;
 
-roots(den);
-pole(gs);
+diferencias = abs(t - 8); % Calcula la diferencia absoluta
+indice = find(diferencias == min(diferencias)); % Encuentra el índice del valor más cercano
 
-[y,t]=step(gs,15);
+%plot(tau,y(indice),'o',DisplayName='Punto 4T estabilizacion');
 
-subplot(2,1,1)
-plot(t,y);
-grid on
-title("Sistema oscilatorio");
-subplot(2,1,2)
-pzmap(gs)
-
-%%
+legend
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
